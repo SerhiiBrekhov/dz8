939891e64df400e5fbed57693f83b8a3657346ad
SerhiiBrekhov <81041094+SerhiiBrekhov@users.noreply.github.com>
2022-09-09 09:16:38 +0000

    Deploying to gh-pages from @ SerhiiBrekhov/dz8@9631f2384432798530581408f84f0647d8075b79 🚀

---
+++ 02-video.593d9a61.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("iframe"),t=new Vimeo.Player(e);console.log(t),t.on("play",(function(){console.log("played the video!")})),t.on("timeupdate",_.throttle((function(e){localStorage.setItem("videoplayer-current-time",e.seconds)}),1e3));var o=localStorage.getItem("videoplayer-current-time");t.setCurrentTime(o).then((function(e){})).catch((function(e){e.name}))}();
+//# sourceMappingURL=02-video.593d9a61.js.map
